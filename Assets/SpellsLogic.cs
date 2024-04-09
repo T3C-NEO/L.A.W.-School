6c83e1253ee19fbd83f0f333529a986cff6c10b7
T3CNEO <neoshon421@gmail.com>
2024-04-09 02:13:30 -0400

    Initial commit

---
+++ Assets/SpellsLogic.cs
@@ -0,0 +1,175 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellsLogic : MonoBehaviour
+{
+
+    public SpriteRenderer arrow0;
+    public SpriteRenderer arrow1;
+    public SpriteRenderer arrow2;
+    public SpriteRenderer arrow3;
+    public SpriteRenderer arrow4;
+    public SpriteRenderer arrow5;
+
+    //creating rotations
+    Quaternion up = new Quaternion(0, 0, 0, 0);
+    Quaternion down = Quaternion.Euler(0, 0, 180);
+    Quaternion left = Quaternion.Euler(0, 0, 90);
+    Quaternion right = Quaternion.Euler(0, 0, 270);
+
+    int place = 0;
+    Color entered = new Color(0.4f, 0.9f, 0, 1);
+    Color clear = new Color(1, 1, 1, 1);
+
+    //creating where the positions are depending on how many arrows there are
+    public List<Vector2> threePos = new List<Vector2>();
+    public List<Vector2> fourPos = new List<Vector2>();
+    public List<Vector2> fivePos = new List<Vector2>();
+    public List<Vector2> sixPos = new List<Vector2>();
+
+    public List<string> spellList = new List<string>();
+
+    string spell;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //positions for a spell with four commands
+        fourPos.Add(new Vector2(-4.5f, 0));
+        fourPos.Add(new Vector2(-1.5f, 0));
+        fourPos.Add(new Vector2(1.5f, 0));
+        fourPos.Add(new Vector2(4.5f, 0));
+
+        //positions for a spell with five commands
+        fivePos.Add(new Vector2(-6, 0));
+        fivePos.Add(new Vector2(-3, 0));
+        fivePos.Add(new Vector2(0, 0));
+        fivePos.Add(new Vector2(3, 0));
+        fivePos.Add(new Vector2(6, 0));
+
+        spellList.Add("Presto");
+        spellList.Add("Shocking Grasp");
+        spellList.Add("Dominate Person");
+
+
+
+    }
+
+    void rollSpell()
+    {
+        place = 0;
+        int i = Random.Range(0, spellList.Count);
+
+        arrow0.color = clear;
+        arrow1.color = clear;
+        arrow2.color = clear;
+        arrow3.color = clear;
+        arrow4.color = clear;
+        arrow5.color = clear;
+
+        spell = spellList[i];
+        if (spell == "Presto" || spell == "Shocking Grasp")
+        {
+            arrow0.gameObject.SetActive(true);
+            arrow1.gameObject.SetActive(true);
+            arrow2.gameObject.SetActive(true);
+            arrow3.gameObject.SetActive(true);
+            arrow4.gameObject.SetActive(false);
+            arrow5.gameObject.SetActive(false);
+            arrow0.gameObject.transform.position = fourPos[0];
+            arrow1.gameObject.transform.position = fourPos[1];
+            arrow2.gameObject.transform.position = fourPos[2];
+            arrow3.gameObject.transform.position = fourPos[3];
+        }
+        if (spell == "Dominate Person")
+        {
+            arrow0.gameObject.SetActive(true);
+            arrow1.gameObject.SetActive(true);
+            arrow2.gameObject.SetActive(true);
+            arrow3.gameObject.SetActive(true);
+            arrow4.gameObject.SetActive(true);
+            arrow5.gameObject.SetActive(false);
+            arrow0.gameObject.transform.position = fivePos[0];
+            arrow1.gameObject.transform.position = fivePos[1];
+            arrow2.gameObject.transform.position = fivePos[2];
+            arrow3.gameObject.transform.position = fivePos[3];
+            arrow4.gameObject.transform.position = fivePos[4];
+        }
+
+        if (spell == "Presto")
+        {
+            arrow0.gameObject.transform.rotation = up;
+            arrow1.gameObject.transform.rotation = down;
+            arrow2.gameObject.transform.rotation = right;
+            arrow3.gameObject.transform.rotation = right;
+        }
+        if (spell == "Shocking Grasp")
+        {
+            arrow0.gameObject.transform.rotation = right;
+            arrow1.gameObject.transform.rotation = right;
+            arrow2.gameObject.transform.rotation = left;
+            arrow3.gameObject.transform.rotation = left;
+        }
+        if (spell == "Dominate Person")
+        {
+            Debug.Log(spell);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rollSpell();
+        }
+        if (spell == "Presto")
+        {
+            if (Input.GetKeyDown("w") && place == 0)
+            {
+                arrow0.color = entered;
+                place += 1;
+            }
+            else if (Input.GetKeyDown("s") && place == 1)
+            {
+                arrow1.color = entered;
+                place += 1;
+            }
+            else if (Input.GetKeyDown("d") && place == 2)
+            {
+                arrow2.color = entered;
+                place += 1;
+            }
+            else if (Input.GetKeyDown("d") && place == 3)
+            {
+                arrow3.color = entered;
+                place += 1;
+            }
+        }
+        if (spell == "Shocking Grasp")
+        {
+            if (Input.GetKeyDown("d") && place == 0)
+            {
+                arrow0.color = entered;
+                place += 1;
+            }
+            else if (Input.GetKeyDown("d") && place == 1)
+            {
+                arrow1.color = entered;
+                place += 1;
+            }
+            else if (Input.GetKeyDown("a") && place == 2)
+            {
+                arrow2.color = entered;
+                place += 1;
+            }
+            else if (Input.GetKeyDown("a") && place == 3)
+            {
+                arrow3.color = entered;
+                place += 1;
+            }
+        }
+    }
+}
